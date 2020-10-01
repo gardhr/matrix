8a5835d6a8370f0bd5268aab1042a76b0227ec50
gardhr <24468810+gardhr@users.noreply.github.com>
2020-10-01 10:15:10 -0500

    update

--- solve.cpp
+++ solve.cpp
@@ -61,8 +61,7 @@ int main(int argc, char** argv)
   if(outfile == "stdout")
    pout = &cout;
   else
-   out.open(outfile);
-   
+   out.open(outfile); 
   string input;
   vector<double> data;
   size_t columns = 0;
